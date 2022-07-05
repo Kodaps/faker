ef08889f8cc7321e73a067b7f6d571d08f101cc9
David Hockley <david.hockley@gmail.com>
2022-07-05 06:38:11 +0200

    initial commit

---
+++ src/faker.ts
@@ -0,0 +1,9 @@
+export const faker = () => {
+
+  return [
+    {name: "David", lastname: "Kodaps"},
+    {name: "Mike", lastname: "Moran"},
+    {name: "Will", lastname: "Smith"},
+    {name: "Steve", lastname: "Wilson"},
+  ];
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { faker } from "./faker";
+import {swapi, SwapiCharacter} from "./swapi";
+
+
+export {faker, swapi, SwapiCharacter};

e13d6f364beb745ab1f9452f384ed7d1b89dc39c
David Hockley <david.hockley@gmail.com>
2022-08-05 06:10:48 +0200

    clean up tests

--- __test__/basic.test.ts
+++ __test__/basic.test.ts
@@ -5,6 +5,6 @@ import { swapi } from '../src/index';
 
 describe('Basic content', () => {
   it('Should have 30 elements', () => {
-    expect(swapi.length).toBe(31);
+    expect(swapi.length).toBe(30);
   });
 });
